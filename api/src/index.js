11f919502a62fe642ec8d6eaccb6cab3f93978d8
dymafr <contact@dyma.fr>
2022-08-27 13:11:32 +0200

    maj React18

--- api/src/index.js
+++ api/src/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 let clientDb;
 let count;
-const MongUrl = process.env.NODE_ENV === 'production' ? 
-`mongodb://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PWD }@db` : 
-`mongodb://db`
+const MongUrl =
+  process.env.NODE_ENV === 'production'
+    ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@db`
+    : `mongodb://db`;
 
 MongoClient.connect(MongUrl, { useUnifiedTopology: true }, (err, client) => {
   if (err) {
@@ -12,22 +13,28 @@ MongoClient.connect(MongUrl, { useUnifiedTopology: true }, (err, client) => {
   } else {
     console.log('CONNEXION DB OK !');
     clientDb = client;
-    count = client.db('test').collection("count");
+    count = client.db('test').collection('count');
   }
 });
 
 const app = express();
 
 app.get('/api/count', (req, res) => {
-  count.findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true }).then((doc) => {
-    const count = doc.value;
-    res.status(200).json(count.count);
-  })
-})
+  count
+    .findOneAndUpdate(
+      {},
+      { $inc: { count: 1 } },
+      { returnNewDocument: true, upsert: true }
+    )
+    .then((doc) => {
+      const count = doc.value;
+      res.status(200).json(count ? count.count : 0);
+    });
+});
 
 app.all('*', (req, res) => {
   res.status(404).end();
-})
+});
 
 const server = app.listen(80);
 
@@ -42,5 +49,5 @@ process.addListener('SIGINT', () => {
         process.exit(0);
       }
     }
-  })
-})
+  });
+});
